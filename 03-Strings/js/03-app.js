60cd71531ecdb805ff9b19a3052cc7f71d325aad
Jorvict Piña <jorvict15@gmail.com>
2021-08-30 00:19:29 -0500

    Se culmina clase 3 de seccion 3

---
+++ 03-Strings/js/03-app.js
@@ -0,0 +1,15 @@
+// Concatenar un String y Template Strings
+const producto = 'Monitor de 20 Pulgadas'; 
+const precio = '30 USD';
+
+// console.log(producto.concat(precio)); // Metodo para concatenar 2 elementos
+// console.log(producto.concat('En descuento'));
+
+
+// console.log(producto + "con un precio de: " + precio);
+console.log("El producto " + producto + " tiene un precio de " + precio);
+console.log("El producto" , producto , "tiene un precio de" , precio);
+
+
+// Template string, es la mas recomendable, se debe usar Backtick (Alt + 92)
+console.log(`El producto ${producto} tiene un precio de $${precio}`);
