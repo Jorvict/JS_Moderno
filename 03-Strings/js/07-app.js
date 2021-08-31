4e61ea05c472153bd56c755b08174a507e3cbaec
Jorvict Piña <jorvict15@gmail.com>
2021-08-31 00:13:40 -0500

    Se finaliza clase 7 seccion 3

---
+++ 03-Strings/js/07-app.js
@@ -0,0 +1,12 @@
+// String Methods - Convertir a Mayusculas o Minusculas
+const producto = 'Monitor de 20 Pulgadas';
+console.log(producto.toUpperCase());
+console.log(producto.toLowerCase());
+
+
+const email = 'CORREO@CORREO.COM';
+console.log(email.toLowerCase());
+
+const precio = 300;
+console.log(precio); // Si esta de color azul en la consola, es un dato numerico
+console.log(precio.toString());
