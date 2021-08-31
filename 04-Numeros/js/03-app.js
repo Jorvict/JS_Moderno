0dcd4a0f12bf7f07ca4c548f09a41d394e406822
Jorvict Piña <jorvict15@gmail.com>
2021-08-31 11:08:49 -0500

    Se finaliza clase 3 seccion 4

---
+++ 04-Numeros/js/03-app.js
@@ -0,0 +1,41 @@
+// El Objeto Math
+let resultado;
+
+// PI
+resultado = Math.PI;
+
+// Redondear
+resultado = Math.round(2.8);
+resultado = Math.round(2.2);
+resultado = Math.round(2.6);
+resultado = Math.round(2.5);
+
+// Redondear hacia arriba
+resultado = Math.ceil(2.1);
+
+// Redondear hacia abajo (truncar)
+resultado = Math.floor(2.9);
+
+// Raiz Cuadrada
+resultado = Math.sqrt(144);
+
+// Valor absoluto (Obtiene el valor absoluto de un número aunque sea negativo)
+resultado = Math.abs(-500);
+
+// Potencia
+resultado = Math.pow(2, 4);
+
+// Minimo
+resultado = Math.min(3, 5, 2, 1, 12, -3);
+
+// Maximo
+resultado = Math.max(3, 5, 2, 1, 12, -3);
+
+// Aleatorio
+resultado = Math.random();
+
+// Aleatorio dentro de un rango
+resultado = Math.floor(Math.random() * 30); // Valor de 0 a 30
+
+
+console.log(resultado);
