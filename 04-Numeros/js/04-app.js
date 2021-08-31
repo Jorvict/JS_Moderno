f6c14ff6b83d22054bd852ca25e7ba23da6da224
Jorvict Piña <jorvict15@gmail.com>
2021-08-31 11:17:29 -0500

    Se finaliza clase 4 seccion 4

---
+++ 04-Numeros/js/04-app.js
@@ -0,0 +1,17 @@
+// El Orden de las operaciones
+let resultado;
+
+// Multiplicaciones y divisiones se ejecutan primero
+// Sumas y restas se ejecutan después
+
+resultado = 20 + 30 * 2; // Da 80, 30 * 2 = 60 + 20 = 80
+
+resultado = (20 + 30) * 2; // Al tener parentesis, primero suma y luego multiplica
+
+// 20% de descuento en tu carrito de compras
+resultado = (20 + 30 + 40 + 50 + 60) * .2;
+
+// Impuesto 16%
+resultado = (20 + 30) * 1.16;
+
+console.log(resultado);
