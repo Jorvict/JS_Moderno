b52ca681db64cd10631c05e05ad958dc07b7152e
Jorvict Piña <jorvict15@gmail.com>
2021-09-01 10:31:57 -0500

    Se finaliza clase 2 sección 6

---
+++ 06-Booleans/js/02-app.js
@@ -0,0 +1,8 @@
+// Más sobre Comparar Booleans
+const Boolean1 = true;
+const Boolean2 = false;
+
+console.log(Boolean1 == Boolean2); // Retorna False
+
+console.log(Boolean1 === true); // Retorna True
+console.log(Boolean1 === 'true'); // Retorna False
