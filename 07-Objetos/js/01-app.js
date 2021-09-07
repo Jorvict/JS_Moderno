72077b297c8d1d901f26163c18028b8ab1526962
Jorvict Piña <jorvict15@gmail.com>
2021-09-07 02:02:08 -0500

    Se finaliza clase 1 seccion 7

--- 07-Objetos/js/01-app.js
+++ 07-Objetos/js/01-app.js
@@ -6,11 +6,11 @@ const precio = 300;
 const disponible = true;
 
 // Un objeto agrupa todo en una sola variable...
-// La sintaxis del objeto es con llaves {}, y las llaves se asigna el valor con :
+// La sintaxis del objeto es con llaves ={}, y las llaves del objeto se les asigna el valor con :
 // Object literal
 const producto = {
     nombre: 'Monitor de 20 Pulgadas',        // Propiedad / Llave del objeto
-    precio: 300,           // Para agregar mas valores es obligatorio usar , al final
+    precio: 300,           // Para agregar mas llaves es obligatorio usar , al final
     disponible: true
 }
 
@@ -19,7 +19,7 @@ console.log(producto);
 /* Resumen: En lugar de crear 3 variables distintas, podemos crear un
 objeto que almacene esos mismos valores, la sintaxis del objeto es que
 al ser declara se colocan llaves {} y sus Propiedades o Llaves del objeto
-se definen con el "nombre de la llave" + ":" + "El valor que se asignará
+se definen con "nombre de la llave" + ":" + "El valor que se asignará
 a la llave", recordar también colocar una coma "," luego de haber definido
 una llave, esto para poder definir llaves adicionales, la última llave del
 objeto no es necesario que tenga una coma*/
