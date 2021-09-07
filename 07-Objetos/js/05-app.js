7ecb02103657d215706b15feeb192a79e55a32a6
Jorvict Piña <jorvict15@gmail.com>
2021-09-07 17:27:53 -0500

    Se añaden practicas a clase 5 seccion 7

--- 07-Objetos/js/05-app.js
+++ 07-Objetos/js/05-app.js
@@ -17,3 +17,37 @@ const producto = {
 console.log(producto);
 
 console.log(producto.informacion.fabricacion.pais);
+
+
+// Objeto dentro de objeto 2
+const localizacion = {
+    continente : 'América',
+    pais : 'Perú',
+    region : {
+        departamento : 'Lima',
+        distrito: 'Lima'
+    },
+    clima: {
+        humedad: '80%',
+        altura: '100m'
+    }
+}
+console.log(localizacion.region.distrito);
+
+
+// Objeto dentro de objeto 3
+const mobiliaria = {
+    inmueble : 'Casa',
+    habitaciones : 3,
+    baños: 2,
+    direccionLocal: {
+        direccion : 'Jr Alonso 817',
+        distrito : 'San Juan',
+        departamento : 'Lima'
+    },
+    direccionInternacional : {
+        pais: 'Lima',
+        continente: 'América del sur'
+    }
+}
+console.log(mobiliaria.direccionLocal.distrito);
