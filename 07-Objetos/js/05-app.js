84513bf3e52057862d1e049d994674b991e5edfe
Jorvict Piña <jorvict15@gmail.com>
2021-09-07 16:42:24 -0500

    Se finaliza clase 5 seccion 7

--- 07-Objetos/js/05-app.js
+++ 07-Objetos/js/05-app.js
@@ -1,1 +1,19 @@
 // Objetos dentro de Objetos
+const producto = {
+    nombre: 'Monitor de 20 Pulgadas',
+    precio: 300,
+    disponible: true,
+    informacion: {
+        medidas : {
+            peso : '1kg',
+            medida: '1m'
+        },
+        fabricacion : {
+            pais : 'China'
+        }
+    }
+}
+
+console.log(producto);
+
+console.log(producto.informacion.fabricacion.pais);
