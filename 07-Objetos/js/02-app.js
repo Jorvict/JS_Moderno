e662ec5bcdafe3fb22f8c5a8fe7aa8b3adc25a2e
Jorvict Piña <jorvict15@gmail.com>
2021-09-07 02:17:22 -0500

    Se finaliza clase 2 seccion 7

---
+++ 07-Objetos/js/02-app.js
@@ -0,0 +1,16 @@
+// Como Acceder a los valores de un objeto
+const producto = {
+    nombre: 'Monitor de 20 Pulgadas',
+    precio: 300,
+    disponible: true
+}
+
+console.log(producto);
+
+// Los objetos tienen algo disponible llamado "la sintáxis de punto"
+console.log(producto.nombre);
+
+// Existe una segunda forma que no es común pero en ciertos casos es útil
+// En vez de utilizar el punto, se colocan corchetes y dentro, como string
+// colocamos el nombre de la llave a la que deseamos acceder
+console.log(producto['nombre']);
