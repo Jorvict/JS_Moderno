6e04078120fea8934803bd9ddfca68444aec4942
Jorvict Piña <jorvict15@gmail.com>
2021-09-20 01:37:04 -0500

    Se finaliza clase 3 seccion 9

---
+++ 09-Funciones/js/03-app.js
@@ -0,0 +1,10 @@
+// Algunas funciones nativas en JavaScript
+
+// Hay funciones que son propias del core de JS como por ejemplo
+alert(`Hubo un error`);
+
+prompt(`¿Cuál es tu edad`);
+
+console.log( parseInt('20') );
+
+// Cuando tiene el nombre y luego () es una caracteristica de una función
