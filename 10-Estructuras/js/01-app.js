77000ccb9d4b3b114283bc395d81169b9363ac6f
Jorvict Piña <jorvict15@gmail.com>
2021-09-23 00:08:56 -0500

    Se finaliza clase 1 seccion 10

---
+++ 10-Estructuras/js/01-app.js
@@ -0,0 +1,17 @@
+// Creando un If
+
+/* Se utiliza para ejecutar un código u otro en base a una condición,
+es decir, estructuras condicionales, un ejemplo podría ser al utilizar
+un cajero, al utilizar el cajero se ingresa la tarjeta y se coloca
+la clave, en caso de que la clave sea correcta, podremos utilizar el
+cajero, en caso sea incorrecta el sistema nos mostrará error. Este
+es un ejemplo de condicionales */
+
+const puntaje = 1000;
+
+// Dentro del parentesis va la condicion, sí se cumple, se ejecuta el bloque
+if(puntaje == 1000){
+    console.log('Si es igual...');
+}else{
+    console.log('No es igual...');
+}
