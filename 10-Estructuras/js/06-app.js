ef7f1644ba7d619ea0978e45586e701a608143d9
Jorvict Piña <jorvict15@gmail.com>
2021-09-23 01:43:16 -0500

    Se finaliza clase 6 seccion 10

---
+++ 10-Estructuras/js/06-app.js
@@ -0,0 +1,28 @@
+// El Operador && para revisar que se cumplan 2 o más condiciones
+
+/* El operador && revisa que se cumplan 2 o más condiciones que se
+encuentre dentro del IF, por ejemplo al comprar en Amazon, debes
+tener una cuenta Y tener saldo disponible Y el producto debe estar 
+disponible. Sí alguna de todas las condiciones no se cumple, 
+automaticamente pasa al siguiente if o al else*/
+
+const usuario = true;
+const puedePagar = true;
+
+if(usuario && puedePagar){
+
+    console.log('Si puedes comprar');
+
+} else if(!puedePagar && !usuario){
+
+    console.log('No, no puedes comprar');
+    
+} else if(!usuario){
+
+    console.log('Inicia sesión o regístrate');
+
+} else if(!puedePagar){
+
+    console.log('Fondos insuficientes');
+    
+}
