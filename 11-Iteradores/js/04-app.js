58159eb24f5163aae420cd0a16a87acc955cf029
Jorvict Piña <jorvict15@gmail.com>
2021-12-31 16:17:18 -0500

    Se finaliza clase 4 seccion 11

--- 11-Iteradores/js/04-app.js
+++ 11-Iteradores/js/04-app.js
@@ -16,18 +16,18 @@ i = 0; // Inicializar el While
 
 while(i < 100){ // Condición...
 
-    if( i % 15 == 0){ // Debemos revisar de primero nuestra condición menos común o las que nos interesa más
-        //  if( i % 3 == 0 && i % 5 == 0 ){ Otra alternativa
-            console.log(`Número: ${i}. Fizzbuzz!`);
-        }
-        else if( i % 3 == 0){
-            console.log(`Número: ${i}. Fizz`);
-        }
-        else if( i % 5 == 0){
-            console.log(`Número: ${i}. buzz`);
-        } else{
-            console.log(`Número: ${i}`);
-        }
+    // if( i % 15 == 0){ // Debemos revisar de primero nuestra condición menos común o las que nos interesa más
+    //     //  if( i % 3 == 0 && i % 5 == 0 ){ Otra alternativa
+    //         console.log(`Número: ${i}. Fizzbuzz!`);
+    //     }
+    //     else if( i % 3 == 0){
+    //         console.log(`Número: ${i}. Fizz`);
+    //     }
+    //     else if( i % 5 == 0){
+    //         console.log(`Número: ${i}. buzz`);
+    //     } else{
+    //         console.log(`Número: ${i}`);
+    //     }
 
     i++;
 }
@@ -43,4 +43,17 @@ while(i < 10){
 /* Este ultimo bucle no se ejecuta debido a que la
 condición no se cumple */
 
-/* DEsarrollar ejercicio de pare o none usando while */
+/* Desarrollar ejercicio de pare o none usando while */
+
+i = 1;
+
+while(i < 20){
+
+    if(i % 2 == 0){
+        console.log(`El número ${i} es Par`);
+    } else {
+        console.log(`El número ${i} es Impar`);
+    }
+
+    i++;
+}
