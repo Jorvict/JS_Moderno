5d6f591a9446018303c55474a286b4766b15373d
Jorvict Piña <jorvict15@gmail.com>
2022-01-05 21:11:24 -0500

    Se finaliza repaso clase 3 seccion 12

--- 12-ArrayMethods/js/03-app.js
+++ 12-ArrayMethods/js/03-app.js
@@ -1,6 +1,8 @@
 // .reduce
 // Toma una gran cantidad de datos, los une y entrega el resultado
 
+// Ejemplo de recorrer un array de datos e imprimir cuanto
+// debe pagar el cliente
 const carrito = [
     { nombre: 'Monitor 27 Pulgadas', precio: 500 },
     { nombre: 'Televisión', precio: 100 },
@@ -19,3 +21,20 @@ console.log(total);
 // Con un reduce
 let resultado = carrito.reduce( (total, producto) => total + producto.precio, 0);
 console.log(resultado);
+
+/* En el reduce, se pasan 2 parámetros, el primero es 
+el valor "anterior" y el segundo es el valor actual*/
+
+/* En éste caso ya no amérita colocar el "+=" al 
+almacenar los datos de la suma en el total ya que éste
+viene almacenando el valor previo en cada iteración*/
+
+/* Sí colocar una , y un 0, estarás especificando en
+que valor sobre el cual inicia el total*/
+
+
+// Reduce de manera explicita (la anterior es implicita)
+const resultado2 = carrito.reduce((total, producto) => {           
+    return total + producto.precio 
+}, 0) 
+console.log(resultado2);
