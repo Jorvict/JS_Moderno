bb5907b0c8a2b675fbcf59e5bc449947c799a04f
Jorvict Piña <jorvict15@gmail.com>
2022-01-05 19:59:23 -0500

    Se finaliza clase 1 seccion 12

--- 12-ArrayMethods/js/01-app.js
+++ 12-ArrayMethods/js/01-app.js
@@ -1,3 +1,5 @@
+// Array method .some
+
 const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
 
 const carrito = [
@@ -8,3 +10,28 @@ const carrito = [
     { nombre: 'Teclado', precio: 400 },
     { nombre: 'Celular', precio: 700 },
 ]
+
+// Comprobar si un valor existe en un arreglo con forEach
+meses.forEach( mes => {
+    if(mes === 'Enero'){
+        console.log(`Enero si existe`);
+    }
+})
+
+// Comprobar con un array method
+const resultado = meses.includes('Enero');
+console.log(resultado);
+/* NOTA: .includes solo funciona con arreglos de un
+solo índice */
+
+
+// En un arreglo de objetos se utiliza .some
+const existe = carrito.some( producto => {
+    return producto.nombre === 'Celular'
+    // return producto.nombre === 'Monitor curvo' retorna false ya que no existe
+})
+console.log(existe);
+
+// .some también se puede utilizar con un array de un solo índice
+const exite2 = meses.some( mes => mes === 'Febrero');
+console.log(exite2)
