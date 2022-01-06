95bb971f0fa57a636c35407a3763e5bf8ab680bf
Jorvict Piña <jorvict15@gmail.com>
2022-01-05 20:54:40 -0500

    Se finaliza clase 4 seccion 12

--- 12-ArrayMethods/js/04-app.js
+++ 12-ArrayMethods/js/04-app.js
@@ -1,1 +1,21 @@
 // .filter
+/* Crea un nuevo arreglo en base a la condición que es
+evaluada */
+
+const carrito = [
+    { nombre: 'Monitor 27 Pulgadas', precio: 500 },
+    { nombre: 'Televisión', precio: 100 },
+    { nombre: 'Tablet', precio: 100 },
+    { nombre: 'Audifonos', precio: 300 },
+    { nombre: 'Teclado', precio: 400 },
+    { nombre: 'Celular', precio: 700 },
+]
+
+let resultado;
+
+resultado = carrito.filter( producto => producto.precio > 400 ); // Filtra mayores a 400
+resultado = carrito.filter( producto => producto.precio < 600); // Menores a 600
+resultado = carrito.filter( producto => producto.nombre !== 'Audifonos') // Todos los productos que sean distinto a "audifonos"
+resultado = carrito.filter( producto => producto.nombre === 'Audifonos') // Solo retorna "audifonos"
+
+console.log(resultado)
