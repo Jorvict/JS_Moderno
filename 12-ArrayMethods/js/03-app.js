5b3b9670213fe6436995c6c6d5a2af0d32442b9e
Jorvict Piña <jorvict15@gmail.com>
2022-01-05 22:39:12 -0500

    Se finaliza repaso clase 3 seccion 12

--- 12-ArrayMethods/js/03-app.js
+++ 12-ArrayMethods/js/03-app.js
@@ -29,8 +29,8 @@ el valor "anterior" y el segundo es el valor actual*/
 almacenar los datos de la suma en el total ya que éste
 viene almacenando el valor previo en cada iteración*/
 
-/* Sí colocar una , y un 0, estarás especificando en
-que valor sobre el cual inicia el total*/
+/* Sí colocas una , y un número, estarás especificando en
+que valor sobre el cual inicia el parametro "total"*/
 
 
 // Reduce de manera explicita (la anterior es implicita)
@@ -38,3 +38,29 @@ const resultado2 = carrito.reduce((total, producto) => {
     return total + producto.precio 
 }, 0) 
 console.log(resultado2);
+
+
+// Un ejemplo más corto y simple:
+const numeros = [30, 20, 45, 65];
+const suma = numeros.reduce( (valor_anterior, valor_actual) => valor_anterior + valor_actual, 0);
+console.log(suma); // Imprime 160
+
+// Depuración del reduce
+/*  Primera iteración:  valor_anterior = 0 (Debido a la inicialización en 0, caso contrario inicializaría con "")
+                        valor_actual = 30
+                        0 + 30 = 30
+
+    Segunda iteración:  valor_anterior = 30
+                        valor_actual = 20
+                        30 + 20 = 50
+                        
+    Tercera iteración:  valor_anterior = 50
+                        valor_actual = 45
+                        50 + 45 = 95
+                        
+    Cuarta iteración:   valor_anterior = 95
+                        valor_actual = 65
+                        95 + 65 = 160 */
+
+// Es como si el parametro fuera un contador y el segundo
+// parametro fuera el objeto en sí
