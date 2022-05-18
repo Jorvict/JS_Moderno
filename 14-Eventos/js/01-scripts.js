6b7f42053ed791a3f8d5b77c382c5095688358cd
Jorvict <jorvict15@gmail.com>
2022-05-17 20:28:41 -0500

    Se finalizó clase 1 de Sección 14

---
+++ 14-Eventos/js/01-scripts.js
@@ -0,0 +1,10 @@
+// Detectar cuando el HTML esta listo
+
+console.log(1);
+
+document.addEventListener('DOMContentLoaded', () => { // Este evento se ejecuta automaticamente una vez se ha descargado todo el HTML
+    // console.log('Documento listo');
+    console.log(2);
+});
+
+console.log(3);
