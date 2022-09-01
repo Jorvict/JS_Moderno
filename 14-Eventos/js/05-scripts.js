1b673f6b81c5961bfc0d3ec06ecb6e397eb6340e
Jorvict <jorvict15@gmail.com>
2022-09-01 06:57:43 -0500

    REnudación de curso sección 14

--- 14-Eventos/js/05-scripts.js
+++ 14-Eventos/js/05-scripts.js
@@ -9,8 +9,8 @@ window.addEventListener('scroll', () => {
     // console.log(scrollPX);
 
     const premium = document.querySelector('.premium');
-    // const ubicacion = premium.getBoundingClientRect(); // Nos muestra a que distancia se encuentra el elemento (tanto del top como del bottom) - (Mientras mayor sea el valor de TOP, mas abajo esta el elemento en comparacion a nuestro scroll actual)
-    console.log(ubicacion); // Enviar la ubicación a la consola para ir debuggeando
+    const ubicacion = premium.getBoundingClientRect(); // Nos muestra a que distancia se encuentra el elemento (tanto del top como del bottom) - (Mientras mayor sea el valor de TOP, mas abajo esta el elemento en comparacion a nuestro scroll actual)
+    // console.log(ubicacion); // Enviar la ubicación a la consola para ir debuggeando
 
     if(ubicacion.top < 885){
         console.log('El elemento ya está visible');
