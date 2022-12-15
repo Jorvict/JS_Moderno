a23eaed679a14b7515a8d448e91dcc8f2148ee3a
Jorvict <jorvict15@gmail.com>
2022-12-14 19:08:06 -0500

    Se finalizó practica 2 Seccion 24

--- 24-POO/js/02-app.js
+++ 24-POO/js/02-app.js
@@ -1,1 +1,49 @@
 // Métodos y métodos estáticos en las classes
+
+class Cliente {
+
+    constructor(nombre, saldo){
+        this.nombre = nombre;
+        this.saldo = saldo;
+    }
+
+    mostrarInformacion(){
+        return `Cliente: ${this.nombre}, tu saldo es de ${this.saldo}`;
+    }
+
+    // Estas propiedades estáticas para mostrarlas no requieren una instancia
+    static bienvenida(){
+        return `Bienvenido al cajero`
+    }
+
+}
+
+const jorvict = new Cliente('Jorvict', 400);
+console.log(jorvict.mostrarInformacion());
+console.log(jorvict);
+
+// No requiere ser instanciada, se deben llamar desde la clase
+console.log(Cliente.bienvenida()); 
+
+// Mostrara error porque el método estático pertenece mas bien a la clase y no
+// al objeto instanciado (jorvict)
+// console.log(jorvict.bienvenida()); 
+
+
+
+// class declaration
+const Cliente2 = class {
+    
+    constructor(nombre, saldo){
+        this.nombre = nombre;
+        this.saldo = saldo;
+    }
+
+    mostrarInformacion(){
+        return `Cliente: ${this.nombre}, tu saldo es de ${this.saldo}`;
+    }
+}
+
+const jorvict2 = new Cliente2('jorvict', 400);
+console.log(jorvict2.mostrarInformacion());
+console.log(jorvict2);
