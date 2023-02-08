6069460e81a0413b8ca3c135223a4811d6ebd13a
Jorvict <jorvict15@gmail.com>
2023-02-08 02:19:09 -0500

    Se finalizó video 204 de sección 27

--- 27-SetsMapsSymbols/js/07-app.js
+++ 27-SetsMapsSymbols/js/07-app.js
@@ -12,6 +12,35 @@ function *crearGenerador(){
     yield true;
 }
 
-const iterador = crearGenerador();
+// const iterador = crearGenerador();
 
-console.log(iterador);
+
+/* Se dice que el generador se queda dormido porque no lo utilizamos
+   Para utilizarlo usaremos .next, pero luego de que lo utilicemos
+   se dormirá nuevamente, tener en cuenta .next es parte del proto
+*/
+// console.log(iterador);
+// console.log(iterador.next().value); // Imprime el primer yield
+// console.log(iterador.next().done);  // Imprime el segundo yield
+// console.log(iterador.next().value); // Imprime el tercer yield
+// console.log(iterador.next());       // Imprime el cuarto yield
+// console.log(iterador.next());       // Imprime undefined pero con done true
+// console.log(iterador); // Imprime closed porque finalizó de iterar los yield
+
+
+// Generador para carrito de compras
+function *generadorCarrito( carrito ){
+
+    for(let i = 0; i < carrito.length; i++){
+        
+        yield carrito[i];
+    }
+}
+
+const carrito = ['Producto 1', 'Producto 2', 'Producto 3'];
+const iterador = generadorCarrito(carrito);
+
+console.log( iterador.next() );
+console.log( iterador.next() );
+console.log( iterador.next() );
+console.log( iterador.next() );
