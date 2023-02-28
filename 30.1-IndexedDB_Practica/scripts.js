c8ec0b0b81ca33d96862ef565b29439d88059aa8
Jorvict <jorvict15@gmail.com>
2023-02-28 02:05:03 -0500

    Se finalizó presentación 1 de práctica independiente

--- 30.1-IndexedDB_Practica/scripts.js
+++ 30.1-IndexedDB_Practica/scripts.js
@@ -59,28 +59,63 @@ if(indexedDB){
         de indexedDB
     */
 
-    // Método que se ejecuta de primero y verifica sí la DB existe
-    //  # Sí no existe lo primero que haces es crear la DB
-    //  # Sí existe entonces éste método no se ejecuta y pasa a onsuccess 
-    //      que consiste en abrir la DB
+    /* Método que se ejecuta de primero y verifica sí la DB existe
+        # Sí no existe lo primero que haces es crear la DB
+        # Sí existe entonces éste método no se ejecuta y pasa a onsuccess 
+          que consiste en abrir la DB
+        
+        NOTA: El onupgradeneeded es un evento que se dispara cuando 
+            se abre una base de datos en IndexedDB y su versión es 
+            mayor a la versión anteriormente abierta. En este evento 
+            se realiza la configuración de la estructura de la base 
+            de datos y se pueden crear o modificar los object 
+            stores y los indices que la componen.
+    */
     request.onupgradeneeded = () =>{
 
         db = request.result;
-        console.log('Create' + db);
-    }
+        console.log(`DB "${db.name}" created`);
 
-    // Sí la DB se creó correctamente siginifica hasta el momento
-    // todo ha ido bien por lo cual se ejecuta el método onsuccess
-    request.onsuccess = () =>{
-        
-        db = request.result;
-        console.log('OPEN' + db);
+        // Crear Object Store, le pasamos como argumento su nombre
+        const objectStore = db.createObjectStore('tasks')
     }
 
     // Método que se ejecuta sí se ha presentado algún error
     request.onerror = (error) =>{
 
-        console.log('Error' + error);
+        console.log('Error: ' + error.target.error.message);
     }
+
+    /*  Sí la DB se creó correctamente significa hasta el momento
+        todo ha ido bien por lo cual se ejecuta el método onsuccess
+
+        NOTA: El onsucess se utiliza para indicar que la base de datos 
+            se ha abierto correctamente y está lista para ser 
+            utilizada. Este evento se dispara una vez que se ha 
+            establecido la conexión con la base de datos, y se 
+            utiliza comúnmente para iniciar transacciones y acceder 
+            a los object stores.
+
+        NOTA 2: El onsuccess solamente se ejecuta a la hora de acceder
+            a la DB por primera vez, una vez que la DB está abierta se
+            podrán crear transacciones de lectura y escritura, y como
+            la DB ya está abierta no es necesario mas acciones para
+            abrirla por lo que el evento onsuccess no se ejecutará de
+            nuevo a no ser que deseemos abrir la DB nuevamente.
+            (Tener en cuenta que sí se volverá a ejecutar cuando
+            refresquemos la página, no es como el onupgradeneeded que
+            puede que no se ejecute incluso al refrescar la página
+            ya que como se mencionó anteriormente, solamente se ejecuta
+            cuando se actualiza la versión de la DB o cuando no existe
+            la DB y es necesario crearla)
+    */
+    request.onsuccess = () =>{
+        
+        db = request.result;
+        console.log(`DB "${db.name}" opened`);
+        console.log(db);
+
+    }
+
 
 }
