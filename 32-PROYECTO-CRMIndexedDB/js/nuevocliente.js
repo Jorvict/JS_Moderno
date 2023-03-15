339e2c7081d2ad6f16ad94ac4f6a277817559959
Jorvict <jorvict15@gmail.com>
2023-03-14 20:56:11 -0500

    Se finalizó practica 2 sección 32

--- 32-PROYECTO-CRMIndexedDB/js/nuevocliente.js
+++ 32-PROYECTO-CRMIndexedDB/js/nuevocliente.js
@@ -1,11 +1,15 @@
 (function(){
 
     let DB;
+    const formulario = document.querySelector('#formulario');
 
     document.addEventListener('DOMContentLoaded', () =>{
 
         // Conectar a la DB
         conectarDB();
+
+        formulario.addEventListener('submit', validarCliente);
+
     });
 
     function conectarDB(){
@@ -18,7 +22,53 @@
         };
         abrirConexion.onsuccess = function(){
 
+            DB = abrirConexion.result;
+        }
+    }
+
+    function validarCliente(e){
+
+        e.preventDefault();
+        
+        // Leer todos los inputs
+        const nombre = document.querySelector('#nombre').value;
+        const email = document.querySelector('#email').value;
+        const telefono = document.querySelector('#telefono').value;
+        const empresa = document.querySelector('#empresa').value;
+    
+        if( nombre === '' || email === '' || telefono === '' || empresa === ''){
+
+            // console.error('Error');
+            imprimirAlerta('Todos los campos son obligatorios', 'error');
             
+            return;
+        }
+    }
+
+
+    function imprimirAlerta(mensaje, tipo){
+
+        const alerta = document.querySelector('.alerta');
+
+        if(!alerta){
+            
+            // Crear la alerta
+            const divMensaje = document.createElement('div');
+            divMensaje.classList.add('px-4', 'py-3', 'rounded', 'max-w-lg', 'mx-auto', 'mt-6', 'text-center', 'border', 'alerta');
+    
+            if( tipo === 'error'){
+                divMensaje.classList.add('bg-red-100', 'border-red-400', 'text-red-700');
+            } else{
+                divMensaje.classList.add('bg-green-100', 'border-green-400', 'text-green-700');
+            }
+    
+            divMensaje.textContent = mensaje;
+    
+            formulario.appendChild(divMensaje);
+    
+            setTimeout(() => {
+                divMensaje.remove();
+            }, 2000);
         }
     }
 })();
