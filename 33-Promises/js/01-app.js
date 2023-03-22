44d22871d5336fef0f0724fc04c458337f907460
Jorvict <jorvict15@gmail.com>
2023-03-22 00:51:58 -0500

    Se realizó practica 1 sección 33

---
+++ 33-Promises/js/01-app.js
@@ -0,0 +1,29 @@
+// Ejemplo de Callbacks
+// Un callback es útil sí se agregó un nuevo registro mientras descargabamos
+// la data ya existente, ya que ese ultimo registro no lo vamos a tener
+
+const paises = ['Francia', 'España', 'Portugal', 'Australia', 'Inglaterra'];
+
+function nuevoPais( pais, callback){
+    
+    setTimeout(() => {
+        
+        paises.push(pais);
+        callback();
+    }, 2000);
+}
+
+function mostrarPaises(){
+
+    setTimeout(() => {
+        
+        paises.forEach( pais =>{
+            console.log(pais);
+        })
+    }, 1000);
+}
+
+mostrarPaises();
+
+// Vuelve a llamar a la función monstrarPaises la cual estará actualizada
+nuevoPais('Alemania', mostrarPaises);
