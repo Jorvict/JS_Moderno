78af231c36223686cae4d3a64df4c9f75d08b364
Jorvict <jorvict15@gmail.com>
2023-03-24 20:53:27 -0500

    se intentó replicar practica Callback hell a Promises

--- 33-Promises/js/04-app.js
+++ 33-Promises/js/04-app.js
@@ -29,3 +29,41 @@ nuevoPais('Alemania')
         console.log(resultado);
         console.log(paises);
     })
+
+
+
+/////////////////////////////////////////////////////////
+// Practica
+
+const paises2 = [];
+
+function mostrarPaises2(){
+
+    console.log(paises2);
+}
+
+
+const nuevoPais2 = (pais) => new Promise( (resolve, reject) =>{
+
+    setTimeout(() => {
+        
+        paises2.push(pais);
+    
+        resolve(`Agregado país ${pais}`);
+    }, 1500);
+
+});
+
+nuevoPais2('Perú')
+    .then( resultado => {
+        console.log(resultado);
+        mostrarPaises2(resultado);
+        return(nuevoPais2('Colombia'));
+    })
+    .then( resultado =>{
+        mostrarPaises2(resultado);
+        return(nuevoPais2('Ecuador'));
+    })
+    .then( resultado =>{
+        mostrarPaises2(resultado);
+    })
