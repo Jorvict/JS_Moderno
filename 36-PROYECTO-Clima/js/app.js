6618eae04e2fedc711f6b30ef1e6943abaf7d366
Jorvict <jorvict15@gmail.com>
2023-03-30 03:16:36 -0500

    Se realizó practica 3 sección 36

---
+++ 36-PROYECTO-Clima/js/app.js
@@ -0,0 +1,108 @@
+const container = document.querySelector('.container');
+const resultado = document.querySelector('#resultado');
+const formulario = document.querySelector('#formulario');
+
+window.addEventListener('load', () =>{
+    formulario.addEventListener('submit', buscarClima);
+})
+
+function buscarClima(e){
+
+    e.preventDefault();
+    
+    // Validar
+    const ciudad = document.querySelector('#ciudad').value;
+    const pais = document.querySelector('#pais').value;
+    
+    if( ciudad === '' || pais === '' ){
+
+        mostrarError('Ambos campos son obligatorios');
+        return;
+    }
+    
+    // Consultaríamos la API
+    consultarAPI(ciudad, pais);
+}
+
+
+function mostrarError(mensaje){
+
+    const alerta = document.querySelector('.bg-red-100');
+
+    if(!alerta){
+
+        // Crear una alerta
+        const alerta = document.createElement('div');
+        alerta.classList.add(   'bg-red-100', 
+                                'border-red-400', 
+                                'text-red-700', 
+                                'px-4', // Padding horizontal
+                                'py-3', // Padding vertical
+                                'rounded', // Esquinas redondeadas
+                                'max-w-md', // Tamaño de la alerta
+                                'mx-auto', // Centrar horizontalmente
+                                'mt-6', // Margin-top 6
+                                'text-center'
+                            )
+    
+        alerta.innerHTML = `
+            <strong class= "font-bold"> ¡Error! </strong>
+            <span class= "block"> ${mensaje} </span>
+        `;
+    
+        container.appendChild(alerta);
+
+        // Eliminar la alerta después de 3 segundos
+        setTimeout(() => {
+            alerta.remove();
+        }, 3000);
+    }
+
+}
+
+function consultarAPI(ciudad, pais){
+
+    const appId = 'd1b4d2889512abfb9ceaebfafd17aca0';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+    
+    fetch(url)
+        .then( respuesta => respuesta.json())
+        .then( datos => {
+
+            limpiarHTML(); // Limpiar el HTML previo
+            if(datos.cod === "404"){
+                mostrarError('Ciudad no encontrada');
+                return;
+            }
+
+            // Imprimir la respuesta en el HTML
+            mostrarClima(datos);
+        })
+}
+
+function mostrarClima(datos){
+
+    const { main: { temp, temp_max, temp_min } } = datos;
+
+    const centigrados = kelvinACentigrados(temp);
+    
+    const actual = document.createElement('p');
+    actual.innerHTML = `${centigrados} &#8451;`;
+    actual.classList.add('font-bold', 'text-6xl');
+
+    const resultadoDiv = document.createElement('div');
+    resultadoDiv.classList.add('text-center', 'text-white');
+    resultadoDiv.appendChild(actual);
+
+    resultado.appendChild(resultadoDiv);
+}
+
+// Función Helper
+const kelvinACentigrados = grados =>  parseInt(grados - 273.15);
+
+function limpiarHTML(){
+
+    while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+    }
+}
