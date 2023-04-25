3e6b0def93558885f3ce463d0cde365aa4a6f9d2
Jorvict <jorvict15@gmail.com>
2023-04-25 06:23:12 -0500

    Se finalizó video 9 sección 37

--- 37-PROYECTO-BuscadorRecetas/js/app.js
+++ 37-PROYECTO-BuscadorRecetas/js/app.js
@@ -2,6 +2,7 @@ function iniciarApp(){
 
     const selectCategorias = document.querySelector('#categorias');
     const resultado = document.querySelector('#resultado');
+    const modal = new bootstrap.Modal('#modal', {});
 
     // En este caso si funciona el evento porque el Select ya existe en el index.html
     // a diferencia de casos anteriores donde el Select se generó con scripting (al vuelo)
@@ -80,6 +81,11 @@ function iniciarApp(){
             const recetaButton = document.createElement('BUTTON');
             recetaButton.classList.add('btn', 'btn-danger', 'w-100');
             recetaButton.textContent = 'Ver Receta';
+            // recetaButton.dataset.bsTarget = "#modal";
+            // recetaButton.dataset.bsToggle = "modal"; // Toogle manda a llamar las funciones de JS de Bootstrap
+            recetaButton.onclick = function(){
+                seleccionarReceta(idMeal);
+            }
 
             // Inyectar en el código HTML
             recetaCardBody.appendChild(recetaHeading);
@@ -92,6 +98,41 @@ function iniciarApp(){
             
             resultado.appendChild(recetaContenedor);
         })
+    }
+
+    function seleccionarReceta(id){
+        
+        const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+
+        fetch(url)
+            .then( respuesta => respuesta.json())
+            .then( resultado => mostrarRecetaModal(resultado.meals[0]))
+    }
+
+    function mostrarRecetaModal(receta){
+
+        const { idMeal, strInstructions, strMeal, strMealThumb} = receta;
+
+        // Añadir contenido al modal
+        const modalTitle = document.querySelector('.modal .modal-title');
+        const modalBody = document.querySelector('.modal .modal-body');
+
+        modalTitle.textContent = strMeal;
+
+        /* InnerHTML es seguro siempre y cuando sepas de donde vienen los datos
+        Sí es un API que tu mismo creaste y sanitizaste los datos, es bastante
+        seguro utilizar innerHTML, sin embargo sí el usuario va a ingresar datos
+        a un formulario Y DE AHÍ MISMO (Formulario) se utilizará el innerHTML, 
+        puede ser bastante inseguro*/
+        modalBody.innerHTML = `
+            <img class="img-fluid" src="${strMealThumb}" alt =" Receta de ${strMeal}" />
+            <h3 class = "my-3"> Instrucciones </h3>
+            <p> ${strInstructions} </p>
+
+        `;
+
+        // Muestra el modal
+        modal.show();
     }
 
     function limpiarHTML(selector){
