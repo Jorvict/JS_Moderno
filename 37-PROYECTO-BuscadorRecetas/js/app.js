ef97efd28344add9ad962dd60d7e9c54470e969a
Jorvict <jorvict15@gmail.com>
2023-04-26 05:47:34 -0500

    Se finalizó video 12 sección 37

--- 37-PROYECTO-BuscadorRecetas/js/app.js
+++ 37-PROYECTO-BuscadorRecetas/js/app.js
@@ -111,8 +111,6 @@ function iniciarApp(){
 
     function mostrarRecetaModal(receta){
 
-        console.log(receta);
-
         const { idMeal, strInstructions, strMeal, strMealThumb} = receta;
 
         // Añadir contenido al modal
@@ -165,6 +163,15 @@ function iniciarApp(){
         btnFavorito.classList.add('btn', 'btn-danger', 'col');
         btnFavorito.textContent = 'Guardar Favorito';
 
+        // LocalStorage
+        btnFavorito.onclick = () =>{
+            agregarFavorito({
+                id: idMeal,
+                titulo: strMeal,
+                img: strMealThumb
+            });
+        }
+
         const btnCerrarModal = document.createElement('BUTTON');
         btnCerrarModal.classList.add('btn', 'btn-secondary', 'col');
         btnCerrarModal.textContent = 'Cerrar';
@@ -177,6 +184,19 @@ function iniciarApp(){
 
         // Muestra el modal
         modal.show();
+    }
+
+    function agregarFavorito(recetaObj){
+
+        /* 
+            ?? es el operador nullish coalescing, también conocido como 
+            función nula, en caso de que la expresión o evaluación del
+            lado izquierdo marque NULL, va a aplicar lo que esté del
+            lado derecho, en este caso asignar por defecto un array vacío
+        */
+        const favoritos = JSON.parse(localStorage.getItem('favoritos')) ?? [];
+        localStorage.setItem('favoritos', JSON.stringify([...favoritos, recetaObj]));
+
     }
 
     function limpiarHTML(selector){
