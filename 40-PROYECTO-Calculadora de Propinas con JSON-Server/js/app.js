8c20c52c8d848615d370bdbb72f53c1298d9aa1e
Jorvict <jorvict15@gmail.com>
2023-05-08 01:25:40 -0500

    Se finalizó video 1 de sección 40

---
+++ 40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
@@ -0,0 +1,13 @@
+/* 
+    En desarrollo a veces el backend puede tardar en generar las APIs de
+    las DB, en esos casos el front puede generar una API "dummy" con la cual
+    poder usar de maqueta y avanzar el desarrollo del front para que una vez el
+    back culmine el desarrollo de la API simplemente sustituyen los endpoints
+*/
+/* 
+    json-server --watch db.json --port 4000 utilizar en la
+    terminal con la ruta donde se encuentra el archivo para
+    ejecutar el servidor json con la DB seleccionada en el
+    peurto indicado, si se utiliza una ruta incorrecta
+    json-server puede generar una db distinta
+*/ 
