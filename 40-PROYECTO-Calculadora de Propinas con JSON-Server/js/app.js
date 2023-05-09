c7f9aabd5fa5ca3142143f5356287236ee6b2c9a
Jorvict <jorvict15@gmail.com>
2023-05-09 11:10:02 -0500

    Se finalizó video 13 de sección 40

--- 40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
+++ 40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
@@ -216,6 +216,9 @@ function agregarPlatillo(producto){
         cliente.pedido = [...resultado];
     }
 
+    // Limpiar el código HTML previo
+    limpiarHTML();
+
     // Mostrar el Resumen
     actualizarResumen();
 }
@@ -225,7 +228,7 @@ function actualizarResumen(){
     const contenido = document.querySelector('#resumen .contenido');
 
     const resumen = document.createElement('DIV');
-    resumen.classList.add('col-md-6');
+    resumen.classList.add('col-md-6', 'card', 'py-5', 'px-3', 'shadow');
 
     // Información de la mesa
     const mesa = document.createElement('P');
@@ -249,7 +252,27 @@ function actualizarResumen(){
     mesa.appendChild(mesaSpan);
     hora.appendChild(horaSpan);
 
+    // Título de la sección
+    const heading = document.createElement('H3');
+    heading.textContent = 'Platillos Consumidos';
+    heading.classList.add('my-4', 'text-center');
+
+    // Iterar sobre el array de pedidos
+
+
     // Renderizar en el contenido
-    contenido.appendChild(mesa);
-    contenido.appendChild(hora);
+    resumen.appendChild(mesa);
+    resumen.appendChild(hora);
+    resumen.appendChild(heading);
+
+    contenido.appendChild(resumen);
+}
+
+function limpiarHTML(){
+
+    const contenido = document.querySelector('#resumen .contenido');
+
+    while( contenido.firstChild ){
+        contenido.removeChild(contenido.firstChild);
+    }
 }
