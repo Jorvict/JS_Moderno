fa997f1f7b7d7604297ced38237ca9fd1b822361
Jorvict <jorvict15@gmail.com>
2023-05-17 03:12:42 -0500

    Se finalizó video 6 de sección 45

--- 45-FunctionalJS/js/05-app.js
+++ 45-FunctionalJS/js/05-app.js
@@ -1,3 +1,4 @@
+// Menos cantidad de código en las funciones
 const carrito = [
     { nombre: 'Monitor 20 Pulgadas', precio: 500},
     { nombre: 'Televisión 50 Pulgadas', precio: 700},
@@ -8,3 +9,13 @@ const carrito = [
     { nombre: 'Bocinas', precio: 300},
     { nombre: 'Laptop', precio: 800},
 ];
+
+// Podemos aprovechar los arrow functions y el return implicito para simplificar
+// Adicionalmente se "sugiere" colocar nombres cortos para los argumentos
+const obtenerNombres = p => p.nombre;
+const resultado = carrito.map( obtenerNombres );
+console.log(resultado);
+
+const mayor400 = p => p.precio > 400;
+const resultadoHOF = carrito.filter(mayor400);
+console.log(resultadoHOF);
