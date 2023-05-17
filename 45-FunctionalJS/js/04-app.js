462877841f58fc9af35e43eacb77d826b3bdc802
Jorvict <jorvict15@gmail.com>
2023-05-17 02:52:54 -0500

    Se finalizó video 5 de sección 45

--- 45-FunctionalJS/js/04-app.js
+++ 45-FunctionalJS/js/04-app.js
@@ -1,3 +1,12 @@
+// .map es muy utilizado en FunctionalJS
+/**
+ * La mayoría de array methods son buenos candidatos para ser utilizados
+ *  en la programación funcional, .forEach y .map son unos de ellos, tener
+ *  en cuenta que .map lo que realizará es crear un nuevo arreglo utilizando
+ *  los valores que sean retornados haciendo uso del returnm mientras
+ *  que forEach lo que hará es simplemente iterar sobre el arreglo.
+ * */ 
+
 const carrito = [
     { nombre: 'Monitor 20 Pulgadas', precio: 500},
     { nombre: 'Televisión 50 Pulgadas', precio: 700},
@@ -8,3 +17,42 @@ const carrito = [
     { nombre: 'Bocinas', precio: 300},
     { nombre: 'Laptop', precio: 800},
 ];
+
+const obtenerNombres = producto =>{
+    return producto.nombre;
+}
+
+const resultado = carrito.map( obtenerNombres );
+console.log(resultado);
+console.log(carrito);
+
+/**
+ * Repaso de algunos Array Methods:
+ * 
+ *  .includes: Validar si un valor existe, solo en arreglos de un solo índice.
+ * 
+ *  .some: Validar sí un valor existe en un arreglo de objetos.
+ * 
+ *  .findIndex: Identificar en qué índice del array se encuentra un elemento.
+ * 
+ *  .reduce: Toma una gran cantidad de datos, los une y entrega el resultado.
+ *      Puede utilizarse para iterar un array y entregar una suma total.
+ * 
+ *  .filter: Itera un arreglo y retorna aquellos datos que cumplan con 
+ *      la condición establecida.
+ * 
+ *  .find: Retorna el primer elemento del array que cumpla con la condición 
+ *      establecida, sí ningún elemento cumple con la condición retornará
+ *      undefined.
+ * 
+ *  .every: Verifica que todos los elementos del array cumplan con la 
+ *      condición establecida, de ser así retornará true, sí alguno de los
+ *      elementos del array no cumple con la condición entonces retornará
+ *      false. Sí lo que se desea es validar que alguno de los elementos
+ *      cumpla con la condición, podemos utilizar .some.
+ * 
+ *  .concat: Concatena uno o mas elementos, pero para esto se sugiere
+ *      utilizar spread operator.
+ * 
+ * Para más detalles y ejemplos visitar sección 12.
+ */
