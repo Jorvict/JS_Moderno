d6c6d3a6f1787a5e88ef0f2788f5a6221e3dd41b
Jorvict <jorvict15@gmail.com>
2023-05-29 05:49:45 -0500

    Se finalizó video 1 de sección 48

---
+++ 48-DesignPatterns/js/01-app.js
@@ -0,0 +1,26 @@
+/**
+ * ¿Qué son los Design Patterns?
+ * Son soluciones típicas a problemas comunes en Desarrollo
+ * de Software, cada patrón es como un plano que se puede
+ * personalizar para resolver un problema de diseño en el
+ * código. (Existen en todos los lenguajes de programación)
+ * 
+ * Beneficios:
+ *      ~ Son soluciones a problemas de diseño de código
+ *      ~ Son soluciones probadas
+ *      ~ Son soluciones conocidas por todos, y evitan la
+ *      forma de escribir código "como cada quien entiende"
+ * 
+ * Categorías de Patrones:
+ *      ~ De creación: Permiten crear objetos y permiten la
+ *      reutilización del código.
+ *      ~ De Estructura: Explican como deben comunicarse los
+ *      objetos y classes en grandes proyectos.
+ *      ~ Comportamiento: Se encargan de como se comportan
+ *      y comunican los objetos.
+ * 
+ * Hay demasiados patrones de diseño, no es necesario
+ * saberlos todos (aún) pero estaremos aprendiendo los que
+ * debemos conocer o son los básicos para cualquier
+ * desarrollador de software.
+ */
