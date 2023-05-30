6335872bac395cc326a9f27e9b5f422e5762606b
Jorvict <jorvict15@gmail.com>
2023-05-30 07:54:22 -0500

    Se finalizó video 6 de sección 48

--- 48-DesignPatterns/js/05-app.js
+++ 48-DesignPatterns/js/05-app.js
@@ -1,1 +1,30 @@
 // Module Pattern - Categoría: De estructuración
+/**
+ * Es el más común de JS, la síntaxis previamente era muy
+ * distinta a la sintaxis actual para importar modules
+ */
+
+// Sintaxis actual de exportar modules luego de ECMASCRIPT
+// const mostrarCliente = nombre => {
+
+//     console.log(nombre);
+// }
+// export default mostrarCliente;
+
+
+// Sintaxis previa a ECMASCRIPT
+const module1 = (function(){
+
+    const nombre = 'Juan';
+
+    function hola(){
+        console.log('hola');
+    }
+
+    return{
+        nombre,
+        hola
+    }
+}) ();
+
+// Se imprime desde el archivo 06
---
+++ 48-DesignPatterns/js/06-app.js
@@ -0,0 +1,6 @@
+/**
+ * console.log(module1.nombre) Ejemplo module 05
+ * module1.hola(); Ejemplo module 05
+ */
+
+
