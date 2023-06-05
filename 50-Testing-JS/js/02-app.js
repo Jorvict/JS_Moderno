351cfcf3cbd76d74b0a610b0e54c346174bbcbc3
Jorvict <jorvict15@gmail.com>
2023-06-05 04:30:22 -0500

    Se finalizó video 3 de sección 50

---
+++ 50-Testing-JS/js/02-app.js
@@ -0,0 +1,41 @@
+// Más funciones a nuestro Framework
+function suma(a, b){
+    return a + b;
+}
+
+function restar(a, b){
+    return a - b;
+}
+
+// Recordar que si resultado y esperado no coinciden, hay algo mal
+let resultado = suma(1, 2);
+let esperado = 3;
+expected(esperado).toBe(resultado);
+
+resultado = restar(10, 5);
+esperado = 5;
+expected(esperado).toBe(resultado);
+expected(esperado).toEqual(resultado);
+
+
+function expected( esperado ){
+
+    return{
+        toBe(resultado){
+
+            if(resultado !== esperado){
+                console.error(`El resultado es diferente a lo esperado, el resultado fue ${resultado} y lo esperado era ${esperado}. La prueba no pasó`);
+            } else{
+                console.log('La prueba pasó correctamente');
+            }
+        },
+        toEqual(resultado){
+
+            if(resultado !== esperado){
+                console.error(`El resultado no es igual a lo esperado, el resultado fue ${resultado} y lo esperado era ${esperado}. La prueba no pasó`);
+            } else{
+                console.log('La prueba pasó correctamente');
+            }
+        }
+    }
+}
