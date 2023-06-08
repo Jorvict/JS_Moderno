4366fd9c2b3627fb69312f954395e6ba3423802f
Jorvict <jorvict15@gmail.com>
2023-06-08 08:02:53 -0500

    Se finalizó video 9 sección 51

--- 51-Testing-Jest/__tests__/citas.js
+++ 51-Testing-Jest/__tests__/citas.js
@@ -4,9 +4,11 @@ import Citas from "../js/classes/Citas";
 describe('Probar la clase de Citas', () => {
 
     const citas = new Citas();
+    const id = Date.now();
 
     test('Agregar una nueva cita', () => {
         const citaObj = {
+            id,
             mascota: 'Hook',
             propietario: 'Jorvict',
             telefono: '987456321',
@@ -15,11 +17,33 @@ describe('Probar la clase de Citas', () => {
             sintomas: 'Solo duerme'
         }
 
-        citaObj.id = Date.now();
-
         citas.agregarCita(citaObj);
 
         // Prueba
+        expect(citas).toMatchSnapshot();
+    });
+
+
+    test('Actualizar cita', () => {
+        const citaActualizada = {
+            id,
+            mascota: 'Nuevo nombre',
+            propietario: 'Jorvict Enrique',
+            telefono: '987456321',
+            fecha: '08-06-2023',
+            hora:'07:40',
+            sintomas: 'Solo duerme'
+        }
+
+        citas.editarCita(citaActualizada);
+
+        expect(citas).toMatchSnapshot();
+    });
+
+
+    test('Eliminar Cita', () => {
+        citas.eliminarCita(id);
+
         expect(citas).toMatchSnapshot();
     });
 });
