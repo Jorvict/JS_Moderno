fa5b689122dc477f19297b474ca0908560503263
Jorvict <jorvict15@gmail.com>
2023-06-12 04:13:26 -0500

    Se finalizó video 1 sección 52

---
+++ 52-Testing-Cypress/notas.js
@@ -0,0 +1,20 @@
+/**
+ * ==========  Qué es Cypress  ==========
+ * Cypress es una herramienta para hacer testing de la "siguiente
+ * generación", ofrece pruebas que antes no se podían hacer.
+ * 
+ * Cuando escribes pruebasde código o de consola, (como con JEST)
+ * no puedes visualizar ni saber que es lo que el usuario
+ * experimenta.
+ * 
+ * La página web donde se puede averigüar mas sobre es en su
+ * página, cypress.io.
+ * 
+ * 
+ * ==========  Tipos de Pruebas en Cypress  ==========
+ * Es considerado end-to-end pero también ofrece pruebas unitarias
+ * y de integración.
+ * 
+ * Funciona en el navegador, por lo tanto puedes probar con Cypress
+ * proyectos hechos en Java, C#, Python, PHP, Node, React o VueJS
+ *  */
